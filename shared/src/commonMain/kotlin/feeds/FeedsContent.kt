0219cbf19c2f260d7dfec94b552f7b0179fc0d4a
SirDennis <lilyngure@gmail.com>
2023-10-24 16:17:59 +0300

    app update

--- shared/src/commonMain/kotlin/feeds/FeedsContent.kt
+++ shared/src/commonMain/kotlin/feeds/FeedsContent.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
@@ -20,6 +21,9 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 
@@ -73,13 +77,15 @@ fun FeedsContent(
             horizontalArrangement = Arrangement.Center
         ) {
             Image(
-                painter = painterResource("img.png"),
+                painter = painterResource("CoffeeCup.png"),
                 contentDescription = "profile Picture",
                 modifier = Modifier
-                    .fillMaxWidth(0.5f)
+                    .fillMaxWidth(1f)
+                    .padding(start = 16.dp, end = 16.dp)
                     .fillMaxHeight(0.4f)
+                    .clip(shape = RoundedCornerShape(16.dp)),
+                contentScale = ContentScale.FillBounds
             )
         }
-
     }
 }
