ce40278c363d5deb4d3247a4682dd43db805f729
Sir Dennis <lilyngure@gmail.com>
2024-02-07 23:57:05 -0800

    to update ios

--- shared/src/iosMain/kotlin/platform/PlatformSpecific.kt
+++ shared/src/iosMain/kotlin/platform/PlatformSpecific.kt
@@ -4,6 +4,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 
 actual open class  PlatformSpecific{
     actual fun loadFiles(callback: (ImageBitmap?) -> Unit) {
+        //convert  an image to a Imagebitmap equivalent in swift
     }
 
     actual fun loadImages(callback: (ImageBitmap?) -> Unit) {
