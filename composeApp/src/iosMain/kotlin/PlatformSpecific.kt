5ea96d9181cc90744c9253185dc605a56e04324c
SirDennis <lilyngure@gmail.com>
2024-04-18 14:46:36 +0300

    ios imagepicker

--- composeApp/src/iosMain/kotlin/PlatformSpecific.kt
+++ composeApp/src/iosMain/kotlin/PlatformSpecific.kt
@@ -1,5 +1,4 @@
 import androidx.compose.ui.graphics.ImageBitmap
-import platform.AVFoundation.AVMediaTypeText
 import platform.AVFoundation.AVMediaTypeVideo
 import platform.AVFoundation.requestAccessForMediaType
 import platform.Foundation.NSURL
@@ -8,33 +7,72 @@ import platform.PhotosUI.PHPickerConfiguration
 import platform.PhotosUI.PHPickerConfigurationAssetRepresentationModeCurrent
 import platform.PhotosUI.PHPickerConfigurationSelectionOrdered
 import platform.PhotosUI.PHPickerFilter
+import platform.PhotosUI.PHPickerResult
 import platform.PhotosUI.PHPickerViewController
+import platform.PhotosUI.PHPickerViewControllerDelegateProtocol
 import platform.UIKit.UIApplication
 import platform.UIKit.UIImagePickerController
+import platform.UIKit.UIImagePickerControllerCameraDevice
 import platform.UIKit.UIImagePickerControllerSourceType
+import platform.darwin.NSObject
+import platform.UniformTypeIdentifiers.UTTypeImage
+import platform.darwin.NSInteger
 
 
 actual open class PlatformSpecific {
     actual fun loadFiles(callback: (ImageBitmap?) -> Unit) {
         //Todo ----
         //convert  an image to a Imagebitmap equivalent in swift
-        val configuration = PHPickerConfiguration(PHPhotoLibrary.sharedPhotoLibrary())
-        val newFilter =
-            PHPickerFilter.anyFilterMatchingSubfilters(
-                listOf(
-                    PHPickerFilter.imagesFilter(),
-                ),
+        fun createPHPickerViewController(
+            delegate: PHPickerViewControllerDelegateProtocol,
+            selectionMode: NSInteger,
+        ): PHPickerViewController {
+            val configuration = PHPickerConfiguration(PHPhotoLibrary.sharedPhotoLibrary())
+            val newFilter =
+                PHPickerFilter.anyFilterMatchingSubfilters(
+                    listOf(
+                        PHPickerFilter.imagesFilter(),
+                    ),
+                )
+            configuration.filter = newFilter
+            configuration.preferredAssetRepresentationMode =
+                PHPickerConfigurationAssetRepresentationModeCurrent
+            configuration.selection = PHPickerConfigurationSelectionOrdered
+            configuration.selectionLimit = selectionMode
+            configuration.preselectedAssetIdentifiers = listOf<Nothing>()
+            val picker = PHPickerViewController(configuration)
+            picker.delegate = delegate
+            return picker
+        }
+
+        val pickerDelegateImage = object : NSObject(), PHPickerViewControllerDelegateProtocol {
+            override fun picker(
+                picker: PHPickerViewController,
+                didFinishPicking: List<*>,
+            ) {
+                picker.dismissViewControllerAnimated(true, null)
+                println("didFinishPickingSelectedImage: $didFinishPicking")
+                didFinishPicking.forEach {
+                    val result = it as? PHPickerResult ?: return@forEach
+                    result.itemProvider.loadFileRepresentationForTypeIdentifier(
+                        typeIdentifier = UTTypeImage.identifier,
+                    ) { url, error ->
+                        if (error != null) {
+                            println("Error: $error")
+                            return@loadFileRepresentationForTypeIdentifier
+                        }
+                        // onResult(listOfNotNull(url?.let(::KmpFile)))
+                    }
+                }
+            }
+        }
+        val imagePicker =
+            createPHPickerViewController(
+                delegate = pickerDelegateImage,
+                selectionMode = 1,
             )
-        configuration.filter = newFilter
-        configuration.preferredAssetRepresentationMode =
-            PHPickerConfigurationAssetRepresentationModeCurrent
-        configuration.selection = PHPickerConfigurationSelectionOrdered
-        configuration.selectionLimit = 1
-        configuration.preselectedAssetIdentifiers = listOf<Nothing>()
-        val picker = PHPickerViewController(configuration)
-        picker.delegate
         UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
-            picker,
+            imagePicker,
             true,
             null,
         )
@@ -42,17 +80,24 @@ actual open class PlatformSpecific {
 
     actual fun loadImages(callback: (ImageBitmap?) -> Unit) {
         val cameraVc = UIImagePickerController()
+        val cameraVcs =
+            UIImagePickerController.isCameraDeviceAvailable(cameraDevice = UIImagePickerControllerCameraDevice.UIImagePickerControllerCameraDeviceRear)
         platform.AVFoundation.AVCaptureDevice.requestAccessForMediaType(mediaType = AVMediaTypeVideo) {
             if (it) {
                 println("Access granted;;;;")
-                //access granted
-//                cameraVc.sourceType =
-//                    UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera
-//                UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
-//                    cameraVc,
-//                    true,
-//                    null,
-//                )
+                if (cameraVcs) {
+                    cameraVc.sourceType =
+                        UIImagePickerControllerSourceType.UIImagePickerControllerSourceTypeCamera
+                    UIApplication.sharedApplication.keyWindow?.rootViewController?.presentViewController(
+                        cameraVc,
+                        true,
+                        null,
+                    )
+                    println("Camera found")
+                } else {
+                    println("Camera not found")
+                }
+
             } else {
 
                 println("Acces denied;;;;;;;")
